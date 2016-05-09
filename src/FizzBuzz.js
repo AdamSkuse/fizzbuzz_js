ece18677d3245f28a149ec9ffd3388460518cec9
rahulrama <rahulrama@gmail.com>
2016-05-09 17:20:42 +0100

    Step 2: Div by 15

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,22 +1,38 @@
-function Player() {
+function FizzBuzz() {
 }
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
+FizzBuzz.prototype.play = function(number) {
+  if (this.isDivisibleby(15,number)) {
+  return 'FizzBuzz';
   }
+}
 
-  this.isPlaying = true;
-};
+FizzBuzz.prototype.isDivisibleby = function(divisor, number) {
+  return number % divisor === 0;
+}
 
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// // };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
+
+
+// function hi(person) {
+//   return 'Hi, ' + person + '!';
+// }
